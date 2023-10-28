71bf1eb1e61208392cb621b784bc557a794e60ef
Cristian Pan <a16003566@alumnos.uady.mx>
2023-10-28 00:06:20 -0600

    feat: :lipstick: Add gallery

--- app/Views/components/gallery.php
+++ app/Views/components/gallery.php
@@ -1,1 +1,16 @@
-<p>Gallery</p>
+<div class="gallery">
+    <?php for($i = 0; $i < 20; $i++):?>
+    <div class="gallery__element">
+        <a href="#" class="gallery__imageContainer">
+            <img class="gallery__image" src="/assets/images/gallery/<?= rand(1,9)?>.jpg" alt="galeria imagen" loading="lazy">
+            <p class="gallery__autor">Diana Vazquez</p>
+        </a>
+
+        <div class="gallery__info">
+            <p class="gallery__description"><span>Lorem ipsum</span> dolor sit amet consectetur sit amet consectetu</p>
+            <a href="#" class="gallery__available">Comprar</a>
+        </div>
+    </div>
+    <?php endfor ?>
+
+</div>
