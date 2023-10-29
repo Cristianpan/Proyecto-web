1c2d4df1d3598176949ee58a1787960c161e57ed
JonathanQu3v3do <A21216398@alumos.uady.mx>
2023-10-29 12:14:48 -0600

    Feat: Add SearchBar

--- app/Views/components/searchBar.php
+++ app/Views/components/searchBar.php
@@ -1,1 +1,52 @@
-<p>Search Bar</p>
+<?php $this->section("content") ?>
+<div class="search">
+    <button class="search__button"><img src="/assets/images/search-icon.svg" class="search__imagen"></button>
+    <input type="text" class="search__input" placeholder="Escriba el nombre de la obra">
+
+</div>
+<div class="select">
+    <div class="custom__select">
+        <select>
+            <option value="opcion1">Opción 1</option>
+            <option value="opcion2">Opción 2</option>
+            <option value="opcion3">Opción 3</option>
+        </select>
+        <div class="select__icon">
+            <img src="/assets/images/arrow-icon.svg">
+        </div>
+    </div>
+    <div class="custom__select">
+        <select>
+            <option value="opcion1">Opción 1</option>
+            <option value="opcion2">Opción 2</option>
+            <option value="opcion3">Opción 3</option>
+        </select>
+        <div class="select__icon">
+            <img src="/assets/images/arrow-icon.svg">
+        </div>
+    </div>
+    <div class="custom__select">
+        <select>
+            <option value="opcion1">Opción 1</option>
+            <option value="opcion2">Opción 2</option>
+            <option value="opcion3">Opción 3</option>
+        </select>
+        <div class="select__icon">
+            <img src="/assets/images/arrow-icon.svg">
+        </div>
+    </div>
+    <div class="custom__select">
+        <select>
+            <option value="opcion1">Opción 1</option>
+            <option value="opcion2">Opción 2</option>
+            <option value="opcion3">Opción 3</option>
+        </select>
+        <div class="select__icon">
+            <img src="/assets/images/arrow-icon.svg">
+        </div>
+    </div>
+</div>
+
+
+
+<?php $this->endSection() ?>
