0beb6530657f34a48839dfc4d133d33d603e9430
Cristian Pan <a16003566@alumnos.uady.mx>
2023-10-28 21:28:20 -0600

    style: :lipstick: Change styles in user data section

--- app/Views/pages/user/index.php
+++ app/Views/pages/user/index.php
@@ -15,43 +15,36 @@
 <?= view("components/navigationBar") ?>
 
 <div class="profile">
-    <img class="profile__background" src="../assets/images/fondo.jpg">
+    <img class="profile__background" src="/assets/images/fondo.jpg">
 
-    <div class="profile__datas">
-        <div class="profile__datas__picture">
-            <img src="../assets/images/porfile.jpg">
+    <div class="profile__content">
+        <div class="profile__picture">
+            <img src="/assets/images/porfile.jpg">
         </div>
-        <h3 class="profile__datas__name">Nombre artista</h3>
-        <div class="profile__datas__info">
-            <div class="profile__datas__info__numbers">
-                <p><b>30</b><br></p>
-                <p>Creaciones</p>
+        <h3 class="profile__name">Lucía Méndez</h3>
+
+        <div class="profile__info-container">
+            <div class="profile__info">
+                <p class="profile__info-text profile__info-text--bolder">30</p>
+                <p class="profile__info-text">Creaciones</p>
             </div>
-            <div class="profile__datas__info__numbers">
-                <p><b>10</b><br></p>
-                <p>Disponible</p>
+            <div class="profile__info">
+                <p class="profile__info-text profile__info-text--bolder">10</p>
+                <p class="profile__info-text">Disponible</p>
             </div>
         </div>
     </div>
-    <div class="profile__editButton">
-        <button>
-            <img src="../assets/images/edit-icon.svg" alt="Descripción de la imagen">
-            <span>Editar Perfil</span>
-        </button>
-    </div>
-
 </div>
 
-<div class="aboutme">
-    <h1 class="aboutme__name">Nombre artista</h1>
-    <p class="aboutme__job">oficio</p>
-    <p class="aboutme__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Et aut alias odio qui ad ut
+<div class="about">
+    <h1 class="about__name">Lucía Méndez</h1>
+    <p class="about__job">Oficio</p>
+    <p class="about__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Et aut alias odio qui ad ut
         maiores ea iusto. Explicabo consequatur unde provident accusantium harum repudiandae amet ipsam enim. Atque,
         quidem? si Lorem ipsum dolor sit amet consectetur adipisicing elit. Et aut alias odio qui ad ut
         maiores ea iusto. Explicabo consequatur unde provident accusantium harum repudiandae amet ipsam enim. Atque,
         quidem? si</p>
 </div>
-
 
 </div>
 <div>
