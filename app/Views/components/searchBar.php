308572e719c50cafd021927d7c0fc8bede9e6bf9
Cristian Pan <a16003566@alumnos.uady.mx>
2023-10-29 19:57:44 -0600

    feat: :lipstick: Changes to search bar styles

--- app/Views/components/searchBar.php
+++ app/Views/components/searchBar.php
@@ -1,52 +1,42 @@
-<?php $this->section("content") ?>
-<div class="search">
-    <button class="search__button"><img src="/assets/images/search-icon.svg" class="search__imagen"></button>
-    <input type="text" class="search__input" placeholder="Escriba el nombre de la obra">
+<div class="container">
+    <div class="search">
+        <button class="search__button"><img src="/assets/images/search-icon.svg" class="search__icon"></button>
+        <input type="search" class="search__input" placeholder="Escriba el nombre de la obra">
+    </div>
 
-</div>
-<div class="select">
-    <div class="custom__select">
-        <select>
+    <div class="filters">
+        <select class="filters__filter">
+            <option value="" selected>Pintura</option>
             <option value="opcion1">Opción 1</option>
             <option value="opcion2">Opción 2</option>
             <option value="opcion3">Opción 3</option>
         </select>
-        <div class="select__icon">
-            <img src="/assets/images/arrow-icon.svg">
-        </div>
-    </div>
-    <div class="custom__select">
-        <select>
+        <select class="filters__filter">
+            <option value="" selected>Estilo</option>
             <option value="opcion1">Opción 1</option>
             <option value="opcion2">Opción 2</option>
             <option value="opcion3">Opción 3</option>
         </select>
-        <div class="select__icon">
-            <img src="/assets/images/arrow-icon.svg">
-        </div>
-    </div>
-    <div class="custom__select">
-        <select>
+        
+        <select class="filters__filter">
+            <option value="" selected>Tema</option>
             <option value="opcion1">Opción 1</option>
             <option value="opcion2">Opción 2</option>
             <option value="opcion3">Opción 3</option>
         </select>
-        <div class="select__icon">
-            <img src="/assets/images/arrow-icon.svg">
-        </div>
-    </div>
-    <div class="custom__select">
-        <select>
+        
+        <select class="filters__filter">
+            <option value="" selected>Material</option>
             <option value="opcion1">Opción 1</option>
             <option value="opcion2">Opción 2</option>
             <option value="opcion3">Opción 3</option>
         </select>
-        <div class="select__icon">
-            <img src="/assets/images/arrow-icon.svg">
-        </div>
+        
+        <select class="filters__filter">
+            <option value="" selected>Profesion</option>
+            <option value="opcion1">Opción 1</option>
+            <option value="opcion2">Opción 2</option>
+            <option value="opcion3">Opción 3</option>
+        </select>
     </div>
 </div>
-
-
-
-<?php $this->endSection() ?>
