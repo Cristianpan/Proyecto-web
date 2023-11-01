13efc81413d3da7633024c57eb89527a315f25d7
Pablo Gamboa <pgamboan@hotmail.com>
2023-10-31 19:01:45 -0600

    Navbar pt 1

--- app/Views/components/navigationBar.php
+++ app/Views/components/navigationBar.php
@@ -1,1 +1,20 @@
-<p>Navbar</p>
+<nav class="navbar">
+    <div class="navbar-left">
+        <a href="inicio.php">Inicio</a>
+    </div>
+    <div class="navbar-center">
+        <a class="logo">Artzone</a>
+    </div>
+    <div class="navbar-right">
+        <a href="carrito.php">
+            <img src="assets\images\carrito.png" alt="Carrito de compras">
+        </a>
+        <div class="perfil">
+            <a href="perfil.php"> 
+                <div class="perfil-circle">
+                    <img src="usuario.jpg" alt="Foto de perfil">
+                </div>
+            </a>
+        </div>
+    </div>
+</nav>
