a353c045473e8f5e1a35840ae18e62f5c9b612a3
Pablo Gamboa <pgamboan@hotmail.com>
2023-11-03 14:32:00 -0600

    navbar 4.0

--- app/Views/components/navigationBar.php
+++ app/Views/components/navigationBar.php
@@ -1,45 +1,42 @@
 <nav class="navbar">
-  <div class="navbar-left">
-        <a href=""><strong>Inicio</strong></a>
-  </div>
-  <div class="navbar-center">
-    <a href=""> ArtZone </a>
-  </div>
-  <div class="navbar-right">
-    <div class="carrito">
-        <a href="carrito.php">
-            <img src="/assets/images/carrito.png" alt="Carrito de compras">
-        </a>
+    <div class="navbar-left">
+        <a href="/auth/login"><strong>Iniciar Sesión</strong></a>
+        <a href="/auth/signup"><strong>Registrarse</strong></a>
     </div>
-    <div class="perfil" id="perfilCircle">
-      <a href="#" id="perfilLink">
-        <div class="perfil-circle">
-          <img src="/assets/images/user_icon.png" alt="Foto de perfil">
+    <div class="navbar-center">
+        <a href="">ArtZone</a>
+    </div>
+    <div class="navbar-right">
+        <div class="carrito">
+            <a href="#">
+                <img src="/assets/images/carrito.png" alt="Carrito de compras">
+            </a>
         </div>
-      </a>
-      <div class="navbar__profile-options" id="profileOptions">
-        <ul>
-          <li><a href="#" class="navbar__link">Editar Perfil</a></li>
-          <li><a href="#" class="navbar__link">Editar Obra</a></li>
-          <li><a href="#" class="navbar__link">Cerrar Sesión</a></li>
-        </ul>
-      </div>
+        <div class="perfil" id="perfilCircle">
+            <div class="perfil-circle" id="perfilButton">
+                <img src="/assets/images/user_icon.png" alt="Foto de perfil">
+                <!-- Dropdown menu (oculto) -->
+                <div class="navbar__profile-options" id="profileOptions">
+                    <ul>
+                        <li><a href="#" class="navbar__link"><strong>Usuario</strong></a></li>
+                        <li><a href="#" class="navbar__link">Editar Perfil</a></li>
+                        <li><a href="#" class="navbar__link">Cerrar Sesión</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
 </nav>
 
 <script>
-  const perfilLink = document.getElementById('perfilLink');
-  const profileOptions = document.getElementById('profileOptions');
+    const perfilButton = document.getElementById("perfilButton");
+    const profileOptions = document.getElementById("profileOptions");
 
-  perfilLink.addEventListener('click', function (e) {
-    e.preventDefault(); 
+    perfilButton.addEventListener("mouseover", function () {
+        profileOptions.style.display = "block";
+    });
 
-    // Muestra las opciones del perfil al hacer clic <img src="/assets/images/logo_artzone.png" alt="Logo Artzone">
-    if (profileOptions.style.display === 'block') {
-      profileOptions.style.display = 'none';
-    } else {
-      profileOptions.style.display = 'block';
-    }
-  });
+    perfilButton.addEventListener("mouseout", function () {
+        profileOptions.style.display = "none";
+    });
 </script>
