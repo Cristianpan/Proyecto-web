31b875407d3240d8b230d6275168520f9bd8134e
Cristian Pan <a16003566@alumnos.uady.mx>
2023-11-24 11:02:37 -0600

    feat: Add redirects

--- app/Controllers/CtrlArtCatalog.php
+++ app/Controllers/CtrlArtCatalog.php
@@ -9,59 +9,80 @@ class CtrlArtCatalog extends BaseController
 {
     public function index()
     {
-        return view("pages/catalog/index"); 
+        return view("pages/catalog/index");
     }
     public function viewCreate()
     {
-        return view("pages/catalog/create"); 
-    }
-
-    public function viewEdit(){
         return view("pages/catalog/create");
     }
 
-    public function create(String $userId){
-        
+    public function viewEdit()
+    {
+        return view("pages/catalog/create");
+    }
+
+    public function create(String $userId)
+    {
         try {
             $itemData = $this->request->getPost();
 
             $response = [
-                'title' => '¡Creación exitosa!', 
-                'message' => 'Los datos de su obra de arte han sido guardado con exito', 
+                'title' => '¡Creación exitosa!',
+                'message' => 'Los datos de su obra de arte han sido guardado con exito',
                 'type' => "success",
             ];
-            return "...create";
+            return redirect()->to("/user/$userId/catalog")->with('response', $response);
         } catch (InvalidDataInputException $th) {
             return redirect()->to("/user/$userId/catalog/create")->withInput();
+        } catch (\Throwable $th) {
+            $response = [
+                'title' => '¡Ops! Ha ocurrido un error',
+                'message' => 'Ha ocurrido un error al guardar los datos de la obra, por favor intente nuevamente.',
+                'type' => "error",
+            ];
+            return redirect()->to("/user/$userId/catalog/create")->withInput()->with('response', $response);
         }
     }
-    
-    public function edit(String $userId,String $artItemId){
+
+    public function edit(String $userId, String $artItemId)
+    {
         try {
             $itemData = $this->request->getPost();
-    
+
             $response = [
-                'title' => '¡Actualización exitosa!', 
-                'message' => 'Los datos de su obra han sido actualizados con exito', 
+                'title' => '¡Actualización exitosa!',
+                'message' => 'Los datos de su obra han sido actualizados con exito',
                 'type' => "success",
             ];
-            return "...edit";
+            return redirect()->to("/user/$userId/catalog")->with('response', $response);
         } catch (InvalidDataInputException $th) {
-            return redirect()->to("/user/$userId/catalog/create")->withInput();
+            return redirect()->to("/user/$userId/catalog/edit/$artItemId")->withInput();
+        } catch (\Throwable $th) {
+            $response = [
+                'title' => '¡Ops! Ha ocurrido un error',
+                'message' => 'Ha ocurrido un error al actualizar los datos de la obra, por favor intente nuevamente.',
+                'type' => "error",
+            ];
+            return redirect()->to("/user/$userId/catalog/edit/$artItemId")->withInput()->with('response', $response);
         }
     }
-    
-    public function delete(String $userId, String $artItemId){
-        
+
+    public function delete(String $userId, String $artItemId)
+    {
         try {
             $response = [
-                'title' => '¡Eliminación exitosa!', 
-                'message' => 'Los datos de la obra han sido eliminados correctamente', 
+                'title' => '¡Eliminación exitosa!',
+                'message' => 'Los datos de la obra han sido eliminados correctamente',
                 'type' => "success",
             ];
-            return "...delete";
+            return redirect()->to("/user/$userId/catalog")->with('response', $response);
         } catch (\Throwable $th) {
-            return redirect()->to("/user/$userId/catalog");
+            $response = [
+                'title' => '¡Ops! Ha ocurrido un error',
+                'message' => 'Ha ocurrido un error al eliminar la obra, por favor intente nuevamente.',
+                'type' => "error",
+            ];
+            return redirect()->to("/user/$userId/catalog/edit/$artItemId")->with('response', $response);
         }
     }
 }
