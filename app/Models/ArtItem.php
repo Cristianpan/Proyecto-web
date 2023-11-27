27fc3593725cd46db4bd99f4e283b19bd05df796
Pablo Gamboa <pgamboan@hotmail.com>
2023-11-27 16:22:34 -0600

    Corregir modelo y validator

--- app/Models/ArtItem.php
+++ app/Models/ArtItem.php
@@ -9,7 +9,7 @@ class ArtItem extends Model
 {
     protected $table            = 'art_items';
     protected $primaryKey       = 'artItemId';
-    protected $allowedFields    = ['userId','title', 'description','artist'];
+    protected $allowedFields    = ['artStyleId','artTypeId','name','materials','shortDescription','description','measurements','localOrigin','onSale','price'];
 
     // Dates
     protected $useTimestamps = false;
--- app/Validators/ArtItemValidation.php
+++ app/Validators/ArtItemValidation.php
@@ -1,26 +1,52 @@
 <?php
 
 namespace App\Validators;
-
 use App\Validators\BaseValidation;
 
 class ArtItemValidation extends BaseValidation
 {
     protected $validationRules = [
-        'title' => 'required',
+        'artStyleId' => 'required|integer',
+        'artTypeId' => 'required|integer',
+        'name' => 'required',
+        'materials' => 'required',
+        'shortDescription' => 'required',
         'description' => 'required',
-        'artist' => 'required|',
+        'measurements' => 'required',
+        'localOrigin' => 'required',
+        'onSale' => 'required|integer',
     ];
 
     protected $validationMessages = [
-        'title' => [
-            'required' => 'El título es obligatorio',
+        'artStyleId' => [
+            'required' => 'La selección de estilo es obligatoria',
+            'integer' => 'No existe el tipo de estilo',
+        ],
+        'artTypeId' => [
+            'required' => 'El selección de tipo es obligatoria',
+            'integer' => 'No existe el tipo de arte',
+        ],
+        'name' => [
+            'required' => 'El nombre de la obra es obligatorio',
+        ],
+        'materials' => [
+            'required' => 'El material utilizado es obligatorio',
+        ],
+        'shortDescription' => [
+            'required' => 'La descripción corta es obligatoria',
         ],
         'description' => [
             'required' => 'La descripción es obligatoria',
         ],
-        'artist' => [
-            'required' => 'El nombre del artista es obligatorio',
+        'measurements' => [
+            'required' => 'Las medidas son obligatorias',
+        ],
+        'localOrigin' => [
+            'required' => 'El lugar de origen es obligatorio',
+        ],
+        'onSale' => [
+            'required' => 'La selección es obligatoria',
+            'integer' => 'No existe esa selección',
         ],
     ];
 }
